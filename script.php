3cf6534d2db4ad559e9d466408be00f6f2e89c56
c0nape <104645213+c0nape@users.noreply.github.com>
2024-06-05 20:57:15 +0300

    Add files via upload

---
+++ script.php
@@ -0,0 +1,30 @@
+<?php
+// ���������, ���� �� ���������� �����
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['places'])) {
+        // �������� ������ �� �����
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+
+        $places = implode(", ", $_POST['places']); 
+
+        
+        $to = "alendk.real@gmail.com";
+        $subject = "Имя: $name";
+        $message = "Email: $email\n";
+
+        $message .= "Места, которые хочет посетить: $places\n";
+
+        // ���������� ���������
+        if (mail($to, $subject, $message)) {
+            echo "<p>Сообщение отправлено!</p>";
+        } else {
+            echo "<p>Возникла какая-то ошибка.</p>";
+        }
+    } else {
+        echo "<p>Не все поля заполнены.</p>";
+    }
+}
+?>
