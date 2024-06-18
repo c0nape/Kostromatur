4ae055c8f7d4cd4d1aa3b5f2d24a1d00fd48b16b
c0nape <104645213+c0nape@users.noreply.github.com>
2024-06-18 14:12:48 +0300

    script.php

--- script.php
+++ script.php
@@ -1,23 +1,23 @@
 <?php
-// ���������, ���� �� ���������� �����
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['places'])) {
-        // �������� ������ �� �����
+        
         $name = $_POST['name'];
         $email = $_POST['email'];
 
         $places = implode(", ", $_POST['places']); 
 
         
-        $to = "alendk.real@gmail.com";
+        $to = "crewkren@ya.ru";
         $subject = "Имя: $name";
         $message = "Email: $email\n";
 
         $message .= "Места, которые хочет посетить: $places\n";
 
-        // ���������� ���������
+        
         if (mail($to, $subject, $message)) {
             echo "<p>Сообщение отправлено!</p>";
         } else {
